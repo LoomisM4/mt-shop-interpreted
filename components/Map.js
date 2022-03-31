45ba02e8a237264f65b8a39307f1f185c508d8a4
LoomisM4 <mail@marcel-wettach.com>
2022-03-31 16:35:15 +0200

    Location services are working now

--- components/Map.js
+++ components/Map.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text} from "react-native";
+import Geolocation from "@react-native-community/geolocation";
 
 export default function Map() {
+    const [text, setText] = useState("Laden...")
+
+    useEffect(() => {
+        Geolocation.setRNConfiguration({authorizationLevel: "whenInUse"})
+    }, [])
+
+    Geolocation.getCurrentPosition(position => {
+        console.log(JSON.stringify(position))
+        const long = position.coords.longitude
+        const lat = position.coords.latitude
+        setText("Long: " + long + ", Lat: " + lat)
+    }, error => setText(error.message), {enableHighAccuracy: true, timeout: 5000})
+
     return (
-        <Text>TODO</Text>
+        <Text>{text}</Text>
     )
 }
