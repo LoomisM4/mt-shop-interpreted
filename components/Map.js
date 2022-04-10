4b430af94f66d096819904ab4a38ef2ed67751ce
LoomisM4 <mail@marcel-wettach.com>
2022-04-10 09:19:48 +0200

    map implemented

--- components/Map.js
+++ components/Map.js
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import {Text} from "react-native";
+import {Text, View, StyleSheet} from "react-native";
+import MapView, {Marker} from 'react-native-maps';
 import Geolocation from "@react-native-community/geolocation";
 
 export default function Map() {
-    const [text, setText] = useState("Laden...")
+    const [lat, setLat] = useState();
+    const [long, setLong] = useState();
+    const [loaded, setLoaded] = useState(false);
 
     useEffect(() => {
         Geolocation.setRNConfiguration({authorizationLevel: "whenInUse"})
@@ -11,12 +14,30 @@ export default function Map() {
 
     Geolocation.getCurrentPosition(position => {
         console.log(JSON.stringify(position))
-        const long = position.coords.longitude
-        const lat = position.coords.latitude
-        setText("Long: " + long + ", Lat: " + lat)
-    }, error => setText(error.message), {enableHighAccuracy: true, timeout: 30000})
+        setLat(position.coords.latitude)
+        setLong(position.coords.longitude)
+        setLoaded(true)
+    }, error => console.log(error.message), {enableHighAccuracy: true, timeout: 30000})
 
-    return (
-        <Text>{text}</Text>
-    )
+    if (loaded) {
+        return (
+            <View style={{flex: 1}}>
+                <MapView style={{...StyleSheet.absoluteFillObject}}
+                         showsUserLocation={true}
+                         initialRegion={{
+                            longitude: long,
+                            latitude: lat,
+                            longitudeDelta: 0.01,
+                            latitudeDelta: 0.01
+                }}>
+                    <Marker coordinate={{
+                        longitude: long - 0.003,
+                        latitude: lat + 0.002,
+                    }}/>
+                </MapView>
+            </View>
+        )
+    } else {
+        return <Text>Lade Position</Text>
+    }
 }
